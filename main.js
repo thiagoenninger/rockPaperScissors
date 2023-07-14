c2532f7afa838651e70b2f7119998a2f2defa578
Thiago Enninger <thiagoenninger@gmail.com>
2023-07-14 14:43:29 -0300

    feita lógica para jogar 1 round do game

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 function getComputerChoice() {
-  let options = ["Rock", "Paper", "Scissors"]
-  let random = Math.floor((Math.random() * options.length))
-  let randomOption = options[random]
-  return randomOption
+  let options = ["Rock", "Paper", "Scissors"];
+  let random = Math.floor(Math.random() * options.length);
+  return randomOption = options[random];
 }
+
+function playGame() {
+  let playerSelection = prompt("Choose one between Rock, Paper or Scissors: ");
+  let computerChoice = getComputerChoice();
+  console.log("Computer choose: " + computerChoice)
+
+  if(playerSelection === computerChoice){
+    console.log("It's a tie!")
+  } else if (playerSelection === "Paper" && computerChoice === "Scissors" || playerSelection === "Scissors" && computerChoice==="Rock" || playerSelection==="Rock" && computerChoice==="Paper") {
+      console.log("You Lose! " + computerChoice + " beats " + playerSelection + ".")
+  } else {
+    console.log("Congratulations! " + playerSelection + " beats " + computerChoice + ".")
+  }
+} 
